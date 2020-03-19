0f6b102b1c2bbfdb2c3d5ce50d012d8935430183
Jaga Apple <jagaapple@uniboar.com>
2020-03-19 20:56:03 +0900

    [Add] Add a non-styled list story

--- stories/3-advanced-examples.stories.tsx
+++ stories/3-advanced-examples.stories.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 
-import { KanbanComponent, TreeComponent } from "./3-advanced-examples";
+import { KanbanComponent, NonStyledComponent, TreeComponent } from "./3-advanced-examples";
 
 storiesOf("3 Advanced Examples", module)
+  .add("Non styled", () => <NonStyledComponent />)
   .add("Tree", () => <TreeComponent />)
   .add("Kanban", () => <KanbanComponent />);
--- stories/3-advanced-examples/index.ts
+++ stories/3-advanced-examples/index.ts
@@ -1,2 +1,3 @@
 export { TreeComponent } from "./tree.component";
+export { NonStyledComponent } from "./non-styled.component";
 export { KanbanComponent } from "./kanban.component";
